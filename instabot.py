1fa558c83c07481373eb05248853889fd8a70803
Shruti Iyyer <shruti.personified@gmail.com>
2017-07-04 09:47:18 +0530

    Files renamed to instabot.py, menu generated

--- instabot.py
+++ instabot.py
@@ -2,9 +2,10 @@ import requests
 
 APP_ACCESS_TOKEN="5683010082.f0c5981.7724a99b4e794e4a8d4976af727fe38d" #my key
 BASE_URL="https://api.instagram.com/v1/"
+MENU_LIST=["Fetch personal information.","Fetch info of a user.","Quit"]
 
 
-def self_info():
+def fetch_self_info():
     req_url=BASE_URL+'users/self/?access_token=%s'%(APP_ACCESS_TOKEN)
     try:
         r=requests.get(req_url)
@@ -12,8 +13,6 @@ def self_info():
     except:
         print "Url request exception occurred!"
 
-
-self_info()
 
 def fetch_uid(username):
     uid=0
@@ -29,7 +28,9 @@ def fetch_uid(username):
         print "Url request exception occurred!"
     return uid
 
-def other_user(uid):
+
+
+def fetch_other_user(uid):
     print uid
     if uid==0:
         print 'Username could not be fetched!'
@@ -50,5 +51,25 @@ def other_user(uid):
         print "exception occurred!"
 
 
-#no sandbox invites successful yet hence my own name for the search
-other_user(fetch_uid("shrutiiyyer"))
+
+
+
+def show_menu():
+    i=0
+    print "Welcome to InstaBot! \n What are you upto today? Let's pick from the menu"
+    while True:
+
+        for i in range(0,len(MENU_LIST)):
+            print '%d. %s'%(i+1,MENU_LIST[i])
+
+        menu_choice=int(raw_input())
+        if menu_choice==1:
+            fetch_self_info()
+        if menu_choice==2:
+            u_name=raw_input("Enter the username to fetch details.")
+            fetch_other_user(fetch_uid(u_name))
+        else:
+            exit(0)
+
+
+show_menu()
