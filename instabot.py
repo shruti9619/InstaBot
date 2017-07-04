c8017bc0dacc813b594a95983e41b05f96a7b0c0
Shruti Iyyer <shruti.personified@gmail.com>
2017-07-04 18:12:37 +0530

    Added latest post like feature

--- instabot.py
+++ instabot.py
@@ -5,7 +5,7 @@ import urllib
 # my key
 APP_ACCESS_TOKEN = "5683010082.f0c5981.7724a99b4e794e4a8d4976af727fe38d"
 BASE_URL = "https://api.instagram.com/v1/"
-MENU_LIST = ["Fetch personal information.","Fetch info of a user.", "Fetch your most recent post", "Fetch most recent posts of a user", "Fetch my most recent posts liked" "Quit"]
+MENU_LIST = ["Fetch personal information.","Fetch info of a user.", "Fetch your most recent post", "Fetch most recent posts of a user", "Fetch my most recent posts liked", "Like most recent post of a user", "Quit"]
 
 
 # method to download files
@@ -44,7 +44,7 @@ def fetch_self_info():
 
 # method to fetch username from the api
 def fetch_uid(username):
-    uid = 0
+    uid = None
     req_url = BASE_URL + 'users/search?q=%s&access_token=%s' % (username,APP_ACCESS_TOKEN)
     try:
         user_info=requests.get(req_url).json()
@@ -62,23 +62,24 @@ def fetch_uid(username):
 def fetch_other_user(uid):
     # print uid for easiness's sake
     print uid
-    if uid == 0:
+    if uid is None:
         print 'Username could not be fetched! \n\n'
-    req_url=BASE_URL+'users/%s?access_token=%s' % (uid, APP_ACCESS_TOKEN)
-    try:
-        r=requests.get(req_url).json()
-        if r['meta']['code'] == 200:
-            if len(r['data']):
-                print 'Username: %s' % (r['data']['username'])
-                print 'No. of followers: %s' % (r['data']['counts']['followed_by'])
-                print 'No. of people he/she is following: %s' % (r['data']['counts']['follows'])
-                print 'No. of posts: %s' % (r['data']['counts']['media'])
+    else:
+        req_url=BASE_URL+'users/%s?access_token=%s' % (uid, APP_ACCESS_TOKEN)
+        try:
+            r=requests.get(req_url).json()
+            if r['meta']['code'] == 200:
+                if len(r['data']):
+                    print 'Username: %s' % (r['data']['username'])
+                    print 'No. of followers: %s' % (r['data']['counts']['followed_by'])
+                    print 'No. of people he/she is following: %s' % (r['data']['counts']['follows'])
+                    print 'No. of posts: %s' % (r['data']['counts']['media'])
+                else:
+                    print 'There is no data for this user!'
             else:
-                print 'There is no data for this user!'
-        else:
-            print 'Status code other than 200 received!'
-    except:
-        print "exception occurred!"
+                print 'Status code other than 200 received!'
+        except:
+            print "exception occurred!"
 
 
 # method to fetch self posts
@@ -91,19 +92,58 @@ def fetch_self_posts(num_posts):
 # method to fetch others posts
 def fetch_other_posts(user_name, num_posts):
     uid = fetch_uid(user_name)
-    req_url = BASE_URL + "users/%s/media/recent/?access_token=%s&count=%s" % (uid, APP_ACCESS_TOKEN, str(num_posts))
+    if uid is not None:
+        req_url = BASE_URL + "users/%s/media/recent/?access_token=%s&count=%s" % (uid, APP_ACCESS_TOKEN, str(num_posts))
 
-    user_media = requests.get(req_url).json()
-    download_method(user_media)
-
+        user_media = requests.get(req_url).json()
+        download_method(user_media)
+    else:
+        print "User doesn't exist!"
 
 # method to fetch most recent post liked by self
 def fetch_most_recent_liked_self(num_posts):
-    #uid = fetch_uid(user_name)
     req_url = BASE_URL + "users/self/media/liked?access_token=%s&count=%s" % (APP_ACCESS_TOKEN, str(num_posts))
 
     r = requests.get(req_url).json()
     download_method(r)
+
+
+def fetch_most_recent_media_id(uid):
+    req_url = BASE_URL + "users/%s/media/recent/?access_token=%s&count=5" % (uid, APP_ACCESS_TOKEN)
+
+    user_media = requests.get(req_url).json()
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            return user_media['data'][0]['id']
+        else:
+            print "Failed to fetch post!"
+    else:
+         print "Failed to fetch post!"
+    return None
+
+
+# method to like the most recent post of a user
+def like_user_post(user_name):
+    uid=fetch_uid(user_name)
+    if uid is None:
+        print "User doesn't exist!"
+        return
+    else:
+        media_id=fetch_most_recent_media_id(uid)
+
+        if media_id is not None:
+            req_url=BASE_URL + "media/%s/likes" % str(media_id)
+            token_payload = {"access_token": APP_ACCESS_TOKEN}
+            r=requests.post(req_url, token_payload).json()
+
+            if r['meta']['code'] != 200:
+                print "Failed to like the post! 11"
+            else:
+                print "Keep liking more!"
+        else:
+            print "Failed to like the post!22"
+    return
+
 
 # method to show menu and take input
 def show_menu():
@@ -138,6 +178,10 @@ def show_menu():
         elif menu_choice == 5:
             num_of_posts = raw_input("Enter the number of posts that you would like to fetch ")
             fetch_most_recent_liked_self(num_of_posts)
+
+        elif menu_choice == 6:
+            user_name = raw_input("Enter the name of user ")
+            like_user_post(user_name)
         else:
             print 'Quitting...'
             exit(0)
